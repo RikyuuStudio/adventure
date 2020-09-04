d0711b2747d2262923ef2f5cf5a3e4ba1653de69
brunocastle <brunocastle89@gmail.com>
2020-09-04 12:21:09 -0300

    rmt 2

--- index.php
+++ index.php
@@ -21,5 +21,3 @@ require_once PROJECT_PATH . 'core/defines.php';
 echo '<a href="test.php" target="_blank">Test Battle</a><hr/><br/>';
 
 require_once PROJECT_PATH . 'modules/action/action_list.php';
-
-echo "isso é um teste";
